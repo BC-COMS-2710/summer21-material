6b7eaf0a2d901c5990950f1f880da438ea10d9b3
Adam Poliak <azpoliak@gmail.com>
2021-05-03 14:00:41 +0000

    Tutorial 1.1 for release

--- tutorial/week1/1.1/tests/q3_1.py
+++ tutorial/week1/1.1/tests/q3_1.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q3_1',
     'points': 1,
-    'suites': [   {   'cases': [   {'code': '>>> assert number_of_vowels(example_string) == (2, 8, 9, 8, 3);\n>>> \n', 'hidden': False, 'locked': False},
+    'suites': [   {   'cases': [   {'code': '>>> assert number_of_vowels(example_string) == (2, 9, 9, 8, 2);\n>>> \n', 'hidden': False, 'locked': False},
                                    {'code': '>>> assert number_of_vowels("a" * 13 + "e"*2 + "z" * 4 + "u"*5) == (13, 2, 0, 0, 5)\n', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
